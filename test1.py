d3f2d1afe26bf018d8642ce7870d7dee4f746240
Temimy <69554542+Temimy@users.noreply.github.com>
2021-04-12 15:51:05 +0300

    First upload

---
+++ test1.py
@@ -0,0 +1,16 @@
+# define function for printing
+def myprint():
+    print("Alaa")
+    print("Alaa")
+    print('Alaa Temimy')
+    print('Fotha Alaa')
+
+
+# calling print function from function main
+def main():
+    myprint()
+
+
+# if condition is ok , then run main function
+if __name__ == "__main__":
+    main()
